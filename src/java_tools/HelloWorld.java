8ed84bac1432ec10f0e620419dab86acf67c621d
Kim Seolyoung <oasissyk@partner.sk.com>
2017-03-21 15:09:37 +0900

    출력 메시지 추가

--- src/java_tools/HelloWorld.java
+++ src/java_tools/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Time is 3:40 PM");
 		System.out.println("Soooooooo Hot!!");
+		System.out.println("Soooooooo Cool!!");
 	}
 }
